64b431726d9c973641775304ebf2eeb4f4965b08
Ciaran <cguhagil@terpmail.umd.edu>
2019-07-11 11:31:59 -0600

    added some animations

--- src/App.js
+++ src/App.js
@@ -6,30 +6,54 @@ import './App.css';
 
 import {Route} from "react-router-dom";
 import {BrowserRouter as Router} from "react-router-dom";
+import Particles from 'react-particles-js'
 
 
 import Nav from './Components/Nav'
-
-import HomePage from './Components/HomePage';
+import HomePage from './Components/HomePage'
+import About from './Components/About'
 import ArtList from './Components/ArtList'
 import ArtistList from './Components/ArtistList'
 import Catalog from './Components/Catalog'
 import Embassy from './Components/Embassy'
 import EventsList from './Components/EventsList'
 
+const brand = { name: "peekaboo", to: "home" }
+
 function App() {
+
+  
+
+
   return (
     <Router>
       <div className="App">
 
-          <Nav />
+          <Nav brand={brand} />
+
+          
         
           <Route exact path="/" component={HomePage}/>
+          <Route path="/about" component={About} />
           <Route path="/art" component={ArtList} />
           <Route path="/artists" component={ArtistList}/>
           <Route path="/events" component={EventsList}/>
           <Route path="/catalog" component={Catalog}/>
           <Route path="/embassy" component={Embassy}/>
+
+          <Particles />
+
+          {/* <Particles 
+                params={{
+                    polygon: {
+                        enable: true,
+                        type: 'inside',
+                        move: {
+                            radius: 10
+                        },
+                        url: 'pathseg.js'
+                    }
+                }} /> */}
         
       </div>
     </Router>
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -5,35 +5,15 @@ import React, { Component } from 'react';
 
 class Nav extends Component {
 
-    state = { isHide: false };
 
-    hideBar = () => {
-       const { isHide } = this.state
-
-       window.scrollY > this.prev ?
-       !isHide && this.setState({ isHide: true })
-       :
-       isHide && this.setState({ isHide: false });
-
-       this.prev = window.scrollY;
-    }
-
-    componentDidMount(){
-        window.addEventListener('scroll', this.hideBar);
-    }
-
-    componentWillUnmount(){
-         window.removeEventListener('scroll', this.hideBar);
-    }
-
+    
 
 
     render() {
-        
-        const classHide = this.state.isHide ? 'hide' : '';
+    
 
         return ( 
-                <div className={`topbar ${classHide}`}>
+                <div>
                     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <a className="navbar-brand" href="/">F=D</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
@@ -41,35 +21,28 @@ class Nav extends Component {
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarColor02">
-                        <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
+                        <ul id="alignright" className="navbar-nav mr-auto">
+                        <li className="nav-item">
                             <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-                            {/* <NavLink className="nav-link" to="/"> Home </NavLink> */}
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/about">About</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="/art">Art</a>
-                            {/* <NavLink className="nav-link" to="/art"> Art </NavLink> */}
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="/artists">Artists</a>
-                            {/* <NavLink className="nav-link" to="/artists"> Artists </NavLink> */}
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="/events">Events</a>
-                            {/* <NavLink className="nav-link" to="/events"> Events </NavLink> */}
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="/catalog">Catalog</a>
-                            {/* <NavLink className="nav-link" to="/catalog"> Catalog </NavLink> */}
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="/embassy">Embassy</a>
-                            {/* <NavLink className="nav-link" to="/embassy"> Embassy </NavLink> */}
                         </li>
-                        {/* <li className="nav-item">
-                            <a className="nav-link" href="#">About</a>
-                            <NavLink className="nav-link" to="/about"> About </NavLink>
-                        </li> */}
                         </ul>
                         {/* <form className="form-inline my-2 my-lg-0">
                             <input className="form-control mr-sm-2" type="text" placeholder="Search" />
