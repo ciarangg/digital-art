a15475aa3e071c15a51c8ea6d14d664ab5c07383
Ciaran <cguhagil@terpmail.umd.edu>
2019-09-19 17:33:20 -0600

    changed favicon

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -33,7 +33,7 @@ class Nav extends Component {
                         </li>
                         <li className="nav-item">
                             {/* <a className="nav-link" href="/art">Art</a> */}
-                            <NavLink className="nav-link" to="/art"> Exhibition </NavLink>
+                            <NavLink className="nav-link" to="/art"> Exhibitions </NavLink>
                         </li>
                         <li className="nav-item">
                             {/* <a className="nav-link" href="/artists">Artists</a> */}
