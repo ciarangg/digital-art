646b87791d24d877527b3ede026f8625d1189856
Ciaran <cguhagil@terpmail.umd.edu>
2019-10-28 17:53:04 -0400

    netlify crashed gonna delete and pull repo LOL

--- src/Components/Embassy.js
+++ src/Components/Embassy.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-
 class Embassy extends Component {
 
 
     render() {
 
         return ( <div>
-                    <h1>EMBASSY</h1>
+                    <br />
+                    <p>To find information on other exhibitions and events visit our embassy <a href="https://space-p11.com/" target="_blank">here</a>.</p>
 
                 </div>
         );
